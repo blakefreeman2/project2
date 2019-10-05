a70fdcbf706e01155012e11b4ee83b9c4883ee52
Blake Freeman <blakefreeman2@gmail.com>
2019-10-05 11:34:05 -0500

    ...

--- Solved/Hiking/api/api.py
+++ Solved/Hiking/api/api.py
@@ -1,8 +1,19 @@
+
+import os
+
+
+import pandas as pd
+import numpy as np
+
 import requests
 import json
-import pandas as pd
-from api_key import api_key
+api_key = "200602075-3dd15f21d86fec1206fac0fe2d9117c8"
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
+
 
 lat = "40.0274"
 lon = "-105.2519"
@@ -11,20 +22,24 @@ maxResults = "1000"
 
 url = f"https://www.hikingproject.com/data/get-trails?lat={lat}&lon={lon}9&maxDistance={maxDistance}&maxResults={maxResults}&key={api_key}"
 
-
-response = requests.get(url)
-response_json = response.json()
-
-
-# print(url)
-# print(json.dumps(response_json, indent=4,))
-
-df=pd.DataFrame(response_json['trails'])
-df
-
-
-database_path = "C:/Users/blake/Desktop/Git Hub Repository/Project/Solved/Hiking/db/hiking.sqlite"
+database_path = "C:/Users/blake/Desktop/Git Hub Repository/Project/project2/Solved/Hiking/db/hiking.sqlite"
 engine = create_engine(f"sqlite:///{database_path}")
 conn = engine.connect()
 
-df.to_sql(name='hiking', con=engine, if_exists='append', index=True)
+def return_json(apicall):
+    try:
+        response = requests.get(url)
+
+        response.raise_for_status()
+            # Jsonify Responce
+        json_obj = response.json()
+            # Create Pandas Dataframe
+        df = pd.DataFrame(json_obj['trails'])
+            # Send Pandas Dataframe to DB
+        apicall.df.to_sql(name='hiking', con=engine, if_exists='replace', index=True)
+
+        print(json_obj)
+    except requests.exceptions.HTTPError as e:
+        # if error keep going
+       pass
+
--- Solved/Hiking/app.py
+++ Solved/Hiking/app.py
@@ -4,6 +4,11 @@ import os
 import pandas as pd
 import numpy as np
 
+import requests
+import json
+# from .api_key import api_key
+from sqlalchemy import create_engine
+
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
@@ -28,6 +33,13 @@ db = SQLAlchemy(app)
 
 from .models import hiking
 
+# lat = "40.0274"
+# lon = "-105.2519"
+# maxDistance = "1000"
+# maxResults = "1000"
+
+# url = f"https://www.hikingproject.com/data/get-trails?lat={lat}&lon={lon}9&maxDistance={maxDistance}&maxResults={maxResults}&key={api_key}"
+
 
 # create route that renders index.html template
 @app.route("/")
@@ -35,29 +47,101 @@ def home():
 
     return render_template("index.html")
 
-
-def api_request():
-    return {
-        'url': request.host_url.rstrip('/') + url_for('notes_detail', key=key),
-        'text': notes[key]
-    }
-
-# Query the database and send the jsonified results
-@app.route("/apiCall", methods=["POST"])
-def send():
-    dicbody = request.json()
-    if request.method == "POST":
-   
-
-    return render_template("form.html")
-
-
-@app.route("/api/pals")
+@app.route("/api/hiking")
 def pals():
-    results = db.session.query(hiking.id, hiking.summary).all()
+    results = db.session.query(hiking.id, 
+    hiking.name, 
+    hiking.type, 
+    hiking.summary, 
+    hiking.difficulty, 
+    hiking.stars, 
+    hiking.starVotes, 
+    hiking.location, 
+    hiking.url, 
+    hiking.imgSqSmall,
+    hiking.imgSmall,
+    hiking.imgSmallMed,
+    hiking.imgMedium,
+    hiking.length,
+    hiking.ascent,
+    hiking.descent,
+    hiking.high,
+    hiking.low,
+    hiking.longitude,
+    hiking.latitude, 
+    hiking.conditionStatus,
+    hiking.conditionDetails, 
+    hiking.conditionDate).all()
 
-    return jsonify(results)
+    id = [result[0] for result in results]
+    name  = [result[1] for result in results]
+    type  = [result[2] for result in results]
+    summary  = [result[3] for result in results]
+    difficulty  = [result[4] for result in results]
+    stars  = [result[5] for result in results]
+    starVotes  = [result[6] for result in results]
+    location  = [result[7] for result in results]
+    url = [result[8] for result in results] 
+    imgSqSmall = [result[9] for result in results]
+    imgSmall = [result[10] for result in results]
+    imgSmallMed = [result[11] for result in results]
+    imgMedium = [result[12] for result in results]
+    length = [result[13] for result in results]
+    ascent = [result[14] for result in results]
+    descent = [result[15] for result in results]
+    high = [result[16] for result in results]
+    low = [result[17] for result in results]
+    longitude = [result[18] for result in results]
+    latitude  = [result[19] for result in results]
+    conditionStatus = [result[20] for result in results]
+    conditionDetails  = [result[21] for result in results]
+    conditionDate = [result[22] for result in results]
 
+    hiking_data = [{
+        "id": id,
+        "name": name,
+        "type": type,
+        "summary": summary,
+        "difficulty": difficulty,
+        "stars": stars,
+        "starVotes": starVotes,
+        "location": location,
+        "url": url,
+        "imgSqSmall": imgSqSmall,
+        "imgSmall":imgSmall,
+        "imgSmallMed": imgSmallMed,
+        "imgMedium": imgMedium,
+        "length": length,
+        "ascent": ascent,
+        "descent": descent,
+        "high": high,
+        "low": low,
+        "longitude": longitude,
+        "latitude": latitude,
+        "conditionStatus": conditionStatus,
+        "conditionDetails": conditionDetails,
+        "conditionDate": conditionDate
+    }]
 
+    return jsonify(hiking_data)
+
+# @app.route("/data")
+# def return_json(database):
+#     try:
+#         response = requests.get(url)
+
+#         response.raise_for_status()
+#             # Jsonify Responce
+#         json_obj = response.json()
+#             # Create Pandas Dataframe
+#         df = pd.DataFrame(json_obj['trails'])
+#             # Send Pandas Dataframe to DB
+#         df.to_sql(name='hiking', con=engine, if_exists='replace', index=True)
+
+#         print('it worked?')
+#     except requests.exceptions.HTTPError as e:
+#         # if error keep going
+#        pass
+    
 if __name__ == "__main__":
     app.run()
